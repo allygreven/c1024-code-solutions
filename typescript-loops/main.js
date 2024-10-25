cd3ea5577db43eb013e2a866c029cf57a3711418
Ally Greven <allysongreven@gmail.com>
2024-10-25 18:41:39 +0000

    Completed Typescript Loops Exercise

--- typescript-loops/main.js
+++ typescript-loops/main.js
@@ -31,6 +31,16 @@ function getEvenNumbersToTwenty() {
 }
 console.log('getEvenNumbersToTwenty()', getEvenNumbersToTwenty());
 /// /////////////////////PUT REPEAT WORD FUNCTION HERE//////////////////////////////////
+function repeatWord(word, times) {
+  let count = 1;
+  let repeated = '';
+  while (count <= times) {
+    repeated += word;
+    count++;
+  }
+  return repeated;
+}
+console.log(repeatWord('iloveloops', 27));
 /// ////////////////////////////////////////////////////////////////////
 function logEachCharacter(str) {
   for (let i = 0; i < str.length; i++) {
@@ -48,3 +58,41 @@ function doubleAll(numbers) {
 }
 console.log('doubleAll([3, 4, 5, 6])', doubleAll([3, 4, 5, 6]));
 /// //////////////////////////////SUM ARRAY HERE/////////////////////////////////////
+function sumArray(numbers) {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+}
+/// /////////////////////////////////////////////////////////////////
+function reverseString(str) {
+  let reversed = '';
+  for (const char of str) {
+    reversed += char;
+  }
+  return reversed;
+}
+/// ///////////////////////////////////////////////////////////////////
+const obj = {
+  firstName: 'Ally',
+  favNum: 58,
+  favAnimal: 'moo deng',
+};
+function getKeys(obj) {
+  const keys = [];
+  for (const key in obj) {
+    keys.push(key);
+  }
+  return keys;
+}
+console.log('getKeys: ', getKeys(obj));
+/// /////////////////////////////////////////////////////////////////////////
+function getValues(obj) {
+  const values = [];
+  for (const key in obj) {
+    values.push(obj[key]);
+  }
+  return values;
+}
+console.log('getValues: ', getValues(obj));
--- typescript-loops/main.ts
+++ typescript-loops/main.ts
@@ -32,8 +32,19 @@ function getEvenNumbersToTwenty(): number[] {
 console.log('getEvenNumbersToTwenty()', getEvenNumbersToTwenty());
 /// /////////////////////PUT REPEAT WORD FUNCTION HERE//////////////////////////////////
 
+function repeatWord(word: string, times: number): string {
+  let count: number = 1;
+  let repeated: string = '';
+  while (count <= times) {
+    repeated += word;
+    count++;
+  }
+  return repeated;
+}
+console.log(repeatWord('iloveloops', 27));
+
 /// ////////////////////////////////////////////////////////////////////
-function logEachCharacter(str: string): any {
+function logEachCharacter(str: string): void {
   for (let i = 0; i < str.length; i++) {
     console.log(str[i]);
   }
@@ -50,3 +61,44 @@ function doubleAll(numbers: number[]): number[] {
 }
 console.log('doubleAll([3, 4, 5, 6])', doubleAll([3, 4, 5, 6]));
 /// //////////////////////////////SUM ARRAY HERE/////////////////////////////////////
+function sumArray(numbers: number[]): number {
+  let sum = 0;
+  for (const num of numbers) {
+    sum += num;
+  }
+  return sum;
+}
+
+/// /////////////////////////////////////////////////////////////////
+function reverseString(str: string): string {
+  let reversed = '';
+  for (const char of str) {
+    reversed += char;
+  }
+  return reversed;
+}
+/// ///////////////////////////////////////////////////////////////////
+const obj = {
+  firstName: 'Ally',
+  favNum: 58,
+  favAnimal: 'moo deng',
+};
+
+function getKeys(obj: Record<string, unknown>): string[] {
+  const keys = [];
+  for (const key in obj) {
+    keys.push(key);
+  }
+  return keys;
+}
+console.log('getKeys: ', getKeys(obj));
+/// /////////////////////////////////////////////////////////////////////////
+
+function getValues(obj: Record<string, unknown>): unknown[] {
+  const values = [];
+  for (const key in obj) {
+    values.push(obj[key]);
+  }
+  return values;
+}
+console.log('getValues: ', getValues(obj));
