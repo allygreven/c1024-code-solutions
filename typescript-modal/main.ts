3df284a2a50ee7c434561cfced07c75594d9abd1
Ally Greven <allysongreven@gmail.com>
2024-11-06 18:22:56 +0000

    Completed Typescript Modal Exercise

---
+++ typescript-modal/main.ts
@@ -0,0 +1,16 @@
+const $openModal = document.querySelector('.open-modal');
+if (!$openModal) throw new Error('$openModal query not available');
+
+const $dismissModal = document.querySelector('.dismiss-modal');
+if (!$dismissModal) throw new Error('$dismissModal query not available');
+
+const $dialog = document.querySelector('dialog');
+if (!$dialog) throw new Error('$dialog query not available');
+
+$openModal.addEventListener('click', () => {
+  $dialog.showModal();
+});
+
+$dismissModal.addEventListener('click', () => {
+  $dialog.close();
+});
