a5bc17729476e0919c781d5901d13d48c488ec17
Ally Greven <allysongreven@gmail.com>
2024-11-08 22:55:20 +0000

    Completed Typescript View Swapping Exercise

---
+++ typescript-view-swapping/main.js
@@ -0,0 +1,27 @@
+'use strict';
+const $container = document.querySelector('.tab-container');
+const $tab = document.querySelectorAll('.tab');
+const $view = document.querySelectorAll('.view');
+if (!$container) throw new Error('$container query failed');
+if (!$tab) throw new Error('$tab query failed');
+if (!$view) throw new Error('$view query failed');
+$container.addEventListener('click', (event) => {
+  const $eventTarget = event.target;
+  if ($eventTarget.matches('.tab')) {
+    for (let i = 0; i < $tab.length; i++) {
+      if ($tab[i] === $eventTarget) {
+        $tab[i].className = 'tab active';
+      } else {
+        $tab[i].className = 'tab';
+      }
+    }
+  }
+  const $dataValue = $eventTarget.getAttribute('data-view');
+  for (let i = 0; i < $view.length; i++) {
+    if ($view[i].getAttribute('data-view') === $dataValue) {
+      $view[i].className = 'view';
+    } else {
+      $view[i].className = 'view hidden';
+    }
+  }
+});
