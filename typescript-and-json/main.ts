3b251889a0ee1f8578cfeaeac0957d41ac7bc1ec
Ally Greven <allysongreven@gmail.com>
2024-11-18 19:57:12 +0000

    unfinished typescript and json

--- typescript-and-json/main.ts
+++ typescript-and-json/main.ts
@@ -1,12 +1,12 @@
-const array = {
+const array: { isbn: string; title: string; author: string }[] = {
   isbn: '9798352831533',
   title: 'Never Lie',
   author: 'Frieda McFadden',
 };
 
-console.log('value of isbn:', typeof '9798352831533');
-console.log('value of title:', typeof 'Never Lie');
-console.log('value of author:', typeof 'Frieda McFadden');
+console.log('value of isbn:', typeof isbn);
+console.log('value of title:', typeof title);
+console.log('value of author:', typeof author);
 
 const json = JSON.stringify(array);
 
