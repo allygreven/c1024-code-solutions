882c63604b933214617a63e79e648fe7922f3f43
Ally Greven <allysongreven@gmail.com>
2024-11-21 18:37:53 +0000

    updated typescript-promises

--- typescript-promises/main.ts
+++ typescript-promises/main.ts
@@ -1,5 +1,7 @@
 import { takeAChance } from './take-a-chance.js';
 
-takeAChance('Ally')
+const promise = takeAChance('Ally');
+
+promise
   .then((message) => console.log(message))
   .catch((error) => console.error(`Error: ${error.message}`));
