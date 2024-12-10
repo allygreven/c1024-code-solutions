7992fd9f57fdd9be014610ff35961177906652e1
Ally Greven <allysongreven@gmail.com>
2024-12-10 19:52:29 +0000

    Completed React State Exercise

---
+++ react-state/my-app/src/Description.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+type Props = {
+  text: string[];
+};
+export function Description({ text }: Props) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p onClick={() => setCount(count + 1)}>{text[count]}</p>
+    </>
+  );
+}
