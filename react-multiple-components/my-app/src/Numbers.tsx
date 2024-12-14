19fb599b5ce00b3b4e2ceb38c917c842cb0f789c
Ally Greven <allysongreven@gmail.com>
2024-12-14 01:15:51 +0000

    Phase 2 Completed

--- react-multiple-components/my-app/src/Numbers.tsx
+++ react-multiple-components/my-app/src/Numbers.tsx
@@ -1,11 +1,17 @@
 type Props = {
   count: number;
+  currentIndex: number;
 };
 
-export function Numbers({ count }: Props) {
+export function Numbers({ count, currentIndex }: Props) {
   const numButton = [];
   for (let i = 0; i < count; i++) {
-    numButton.push(<button>{i}</button>);
+    numButton.push(
+      <button
+        style={{ backgroundColor: i === currentIndex ? 'lightpink' : 'white' }}>
+        {i}
+      </button>
+    );
   }
 
   return <>{numButton}</>;
--- react-multiple-components/my-app/src/RotatingBanner.tsx
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -3,18 +3,23 @@ import { Header } from './Header';
 import { Previous } from './Previous';
 import { Numbers } from './Numbers';
 import { Next } from './Next';
+import { useState } from 'react';
 
 type Props = {
   items: string[];
 };
 
 export function RotatingBanner({ items }: Props) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
   return (
     <>
-      <Header item={items[2]} />
-      <Previous />
-      <Numbers count={items.length} />
-      <Next />
+      <div className="rotating-banner">
+        <Header item={items[currentIndex]} />
+        <Previous />
+        <Numbers count={items.length} currentIndex={currentIndex} />
+        <Next />
+      </div>
     </>
   );
 }
