a553ed393434bf4995f23bfe9de75fb13c2475c9
Ally Greven <allysongreven@gmail.com>
2024-12-16 04:31:12 +0000

    started react search exercise

---
+++ react-search/my-app/src/App.tsx
@@ -0,0 +1,7 @@
+import './App.css';
+import './SearchableList';
+// import { SearchableList } from './SearchableList';
+
+export default function App() {
+  return <>{/* <SearchableList /> */}</>;
+}
---
+++ react-search/my-app/src/SearchableList.tsx
@@ -0,0 +1,5 @@
+import './SearchableList.css';
+import './SearchBar';
+import './ItemsList';
+
+export function SearchableList() {}
