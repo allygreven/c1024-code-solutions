d832c78fe00e8bf872ec426c4047b7a048a250dc
Ally Greven <allysongreven@gmail.com>
2025-01-08 22:30:48 +0000

    Letting-Go of DS-Hashtable Exercise

---
+++ ds-hashtable/map-query.ts
@@ -0,0 +1,16 @@
+import { Document, DocumentIndex } from './map-index';
+
+/**
+ * Queries a DocumentIndex and returns all the Documents that contain
+ * any of the words in the query, as a Set.
+ */
+export function queryIndex(index: DocumentIndex, query: string): Set<Document> {
+  const value = new Set<Document>();
+  const words = query.match(/\b(\w+)\b/g) || [];
+  for (const word of words) {
+    const docs = index.get(word.toLowerCase());
+    if (docs) {
+    }
+  }
+  return docs;
+}
